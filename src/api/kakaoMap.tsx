ea78705a87f66e947882527625e71efd8f4e07d3
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2021-06-13 18:45:58 +0900

    redirects

--- src/api/kakaoMap.tsx
+++ src/api/kakaoMap.tsx
@@ -4,30 +4,39 @@ import { AddressFragment } from "../__generated__/AddressFragment";
 
 const Map = styled.div``;
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 interface KakaoMapProp {
   address?: AddressFragment;
 }
 
 export const KakaoMap: React.FC<KakaoMapProp> = ({ address }) => {
-  const kakao = (window as any).kakao;
-
   const mapContainer = document.getElementById("map"); // 지도를 표시할 div
+  var options = {
+    //지도를 생성할 때 필요한 기본 옵션
+    center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    level: 3, //지도의 레벨(확대, 축소 정도)
+  };
 
   // 지도를 생성합니다
-  const map = new kakao.maps.Map(mapContainer);
+  const map = new window.kakao.maps.Map(mapContainer, options);
 
-  const geocoder = new kakao.maps.services.Geocoder();
+  const geocoder = new window.kakao.maps.services.Geocoder();
   if (address) {
     geocoder.addressSearch(
       address.address,
       function (result: any, status: any) {
         // 정상적으로 검색이 완료됐으면
-        if (status === kakao.maps.services.Status.OK) {
-          const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        if (status === window.kakao.maps.services.Status.OK) {
+          const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
 
           // 결과값으로 받은 위치를 마커로 표시합니다
-          const marker = new kakao.maps.Marker({
-            map: map,
+          new window.kakao.maps.Marker({
+            map,
             position: coords,
           });
 
