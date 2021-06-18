ef0120d7a6de00b61d9a16da3565cf72852774a5
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2021-06-18 16:32:28 +0900

    서버 url 수정

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const TOKEN = "TOKEN";
 
 export const siteName = "까비";
 
-// export const URL = "https://cabi-backend.herokuapp.com";
-export const URL = "http://localhost:4000";
+export const URL = "https://cabi-backend.herokuapp.com";
+// export const URL = "http://localhost:4000";
