0c7aa10b7a41ba73eaab6fb9d4c8329fba5806b8
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2021-06-28 16:53:34 +0900

    서버 url 수정

--- apollo.config.js
+++ apollo.config.js
@@ -4,8 +4,8 @@ module.exports = {
     tagName: "gql",
     service: {
       name: "cabi-backend",
-      // url: "https://cabi-backend.herokuapp.com/graphql",
-      url: "http://localhost:4000/graphql",
+      url: "https://cabi-backend.herokuapp.com/graphql",
+      // url: "http://localhost:4000/graphql",
     },
   },
 };
